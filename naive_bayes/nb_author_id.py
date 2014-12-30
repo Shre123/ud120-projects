6d9971e1dc92d083cc3d86a660ac56e2b801fe39
Shre123 <shreshta.s01@gmail.com>
2014-12-30 23:42:45 +0000

    Update nb_author_id.py
    Create classifier, fit it, predict and print he accuracy score

--- naive_bayes/nb_author_id.py
+++ naive_bayes/nb_author_id.py
@@ -30,5 +30,15 @@ features_train, features_test, labels_train, labels_test = preprocess()
 
 
 #########################################################
+from sklearn.metrics import accuracy_score
+from sklearn.naive_bayes import GaussianNB
+clf = GaussianNB()
+clf.fit(features_train,labels_train)
+
+pred = clf.predict(features_test)
+
+accuracy = accuracy_score(pred,labels_test)
+print accuracy
+
 
 
